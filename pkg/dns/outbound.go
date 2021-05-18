e89fde1787510dd1b2d848deeb36eaa44534373f
Charly Molter <charly@koyeb.com>
2021-05-18 16:30:27 +0200

    feat(virtual-outbound): Add possibility to disable old vips allocation
    Signed-off-by: Charly Molter <charly@koyeb.com>

--- pkg/dns/outbound.go
+++ pkg/dns/outbound.go
@@ -32,6 +32,9 @@ func VIPOutbounds(
 	externalServices []*core_mesh.ExternalServiceResource,
 	defaultDomain string,
 ) []*mesh_proto.Dataplane_Networking_Outbound {
+	if len(vips) == 0 {
+		dnsOutboundGeneratorLog.Info("Skipping legacy generation as there are no vips")
+	}
 	type vipEntry struct {
 		ip       string
 		port     uint32
